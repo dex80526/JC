a95c7ae0d281598a4c00be454cd0ec7b1de596b9
Dex Chen <Dexing.Chen@Colorado.EDU>
2017-08-24 10:26:18 -0600

    add lock to stats update

--- stat.go
+++ stat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Stat struct {
@@ -10,18 +11,20 @@ type Stat struct {
 
 }
 
+var lock  sync.Mutex
 
 func NewStat() * Stat {
 	return &Stat{0, 0}
 }
 
-//sync is handeld by channel (todo: we may still need sync for the ops here?)
+//sync
 func(s *Stat) UpdateCounter(atime int64) {
-
+    lock.Lock()
 	var nc = s.Total + 1
 	var nv int64 = s.Total * s.Average  + atime
 	s.Average = nv/nc  //todo do float number div and round to int
 	s.Total = nc
+	lock.Unlock()
 	fmt.Printf("number requests: %d, average time: %d\n", s.Total, s.Average)
 }
 
